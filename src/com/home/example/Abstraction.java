e7a306b7a4ca72e8d48ea9ebaf26134db3991c31
deepikapasam <65802988+deepikapasam@users.noreply.github.com>
2022-01-13 19:41:20 +0530

    Update Abstraction.java

--- src/com/home/example/Abstraction.java
+++ src/com/home/example/Abstraction.java
@@ -2,11 +2,7 @@ package com.home.example;
 
 public abstract class Abstraction {
 
-	void Abstract(String[] args) {
-		// TODO Auto-generated method stub
-		String s = new 
-		
-	}
+	
 	
 	
 	void abstract hello();
